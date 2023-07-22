a96911577f75f80650a4ddf8dfeaf51e04982abc
Alex <cabezacarsialejandro@gmail.com>
2023-07-22 12:37:58 +0200

    [FEAT] Common input text created. useful created at services

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css"; 
+import { InputText } from "../../common/InputText/InputText";
 
 export const Home = () => {
 
-    return(
-        <div className="homeDesign">HOLA</div>
-    )
+   return(
+    <div className="homedesign"></div>
+   )
 
 }
