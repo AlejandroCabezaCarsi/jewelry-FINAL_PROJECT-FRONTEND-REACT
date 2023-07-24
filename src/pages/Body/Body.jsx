3f7a9f7f2e4dc228476652b2a4161cbfce92d465
Alex <cabezacarsialejandro@gmail.com>
2023-07-24 16:07:09 +0200

    [FEAT] Created Profile view (WITHOUT CODE)

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom";
 import { Home } from "../Home/Home";
 import { Login } from "../Login/Login";
 import { Register } from "../Register/Register";
+import { Profile } from "../Profile/Profile";
 
 
 export const Body = () => {
@@ -13,6 +14,7 @@ export const Body = () => {
                 <Route path="/" element={<Home/>} />
                 <Route path="/Login" element={<Login/>} />
                 <Route path="/Register" element={<Register/>} />
+                <Route path="/Profile" element={<Profile    />} />
             </Routes>
         </>
     )
