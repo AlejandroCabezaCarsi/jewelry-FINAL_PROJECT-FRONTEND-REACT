04437f5cf6a8fe75cfc71b3b10bf73bca9a7ac23
Alex <cabezacarsialejandro@gmail.com>
2023-07-30 16:55:33 +0200

    [UPDATE] Updated styles of the Header

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -5,34 +5,50 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "./Header.css";
 import { ChangeViewButton } from "../ChangeViewButton/ChangeViewButton";
+import { useSelector } from "react-redux";
+import { userData } from "../../pages/Login/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
+
+    const dataUser = useSelector(userData)
+
+    const token = dataUser.credentials.token
+
+    console.log(token)
+
+    const navigate = useNavigate()
+
   return (
     <>
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <div className="jewlryLogo" onClick={()=>navigate("/")}></div>
+                <Navbar.Brand href="/">Joyería Sylvie</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto d-flex align-items-center">
                         <div className="navLinksLeft d-flex">
-                            <Nav.Link className="p-4" href="#home">Home</Nav.Link>
-                            <Nav.Link className="p-4" href="#link">Link</Nav.Link>
-                            <NavDropdown className="m-3" title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-
-                            </NavDropdown>
+                            <Nav.Link className="p-4" href="/AllProducts">Productos</Nav.Link>
+                            
                         </div>
                     </Nav>
 
                     <Nav>
 
-                        <div className="navLinksRight">
-                            <div className="PRUEBA p-3"><ChangeViewButton name={"Login"} path={"/Login"} /></div>
-                            <div className="PRUEBA p-3">HOLA</div>
-                        </div>
+
+                        {token !== ""
+                        
+                            ?   <div className="navLinksRight">
+                                    <div className="profileButton p-3" onClick={()=> navigate("/Profile")}></div>
+                                </div>
+
+                            :   <div className="navLinksRight">
+                                    <div className="PRUEBA p-3"><ChangeViewButton name={"Login"} path={"/Login"} /></div>
+                                </div>
+                        
+                        }
+                        
                     </Nav>
 
                 </Navbar.Collapse>
