f2d82543d2b44341de7afba332452c90beb3fcee
Alex <cabezacarsialejandro@gmail.com>
2023-07-30 17:50:41 +0200

    [UPDATE] Updated styles of login view (FINISHED)

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,11 +8,8 @@ import { useDispatch } from "react-redux";
 import { login } from "./userSlice";
 import { getUserInfoByToken, loginMe } from "../../services/apicalls";
 
-
-
 export const Login = () => {
-
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const [credentials, setCredentials] = useState({
     email: "",
@@ -25,6 +22,8 @@ export const Login = () => {
   });
 
   const [welcome, setWelcome] = useState("");
+
+  const [backendError, setBackendError] = useState("");
 
   const inputHandler = (e) => {
     setCredentials((prevState) => ({
@@ -46,116 +45,121 @@ export const Login = () => {
 
   const dispatch = useDispatch();
 
-const logMe = () => {
+  const logMe = () => {
     loginMe(credentials)
       .then((resultado) => {
         const token = resultado.data.token;
-  
+
         getUserInfoByToken(token)
-  .then((userInfo) => {
-      dispatch(
-          login({
-              token:token,
-              name: userInfo.data.data.name,
-              role: userInfo.data.data.role_ID,
-              id: userInfo.data.data.id,
-            })
+          .then((userInfo) => {
+            dispatch(
+              login({
+                token: token,
+                name: userInfo.data.data.name,
+                role: userInfo.data.data.role_ID,
+                id: userInfo.data.data.id,
+              })
             );
-            
+
             setTimeout(() => {
-                navigate("/");
+              navigate("/");
             }, 3500);
 
             //---------------------------------------------------------
             //ARREGLAR WELCOME
             //---------------------------------------------------------
-            
-            setWelcome(`Bienvenid@ de nuevo ${userInfo.data.data.name}`);
-            console.log(welcome)
-  })
-  .catch((error) => {
-    console.log('Error fetching user information:', error);
-  });
 
+            setWelcome(`Bienvenid@ de nuevo ${userInfo.data.data.name}`);
+          })
+          .catch((error) => {
+            console.log("Error fetching user information:", error);
+          });
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        setBackendError("Usuario o contraseña inválido");
+      });
   };
 
-    return(
-        <div className="">
-
-    {welcome !== "" ? (
-            <div className="welcomeMessage"> {welcome} </div>
-          ) : (
-            <div className="form loginDesign">
-              <Container>
-                <Row className=" d-flex justify-content-center flex-fill p-5">
-                <div className="rowLogin">
-                    <Col 
-                    
-                    xs={0}
-                    sm={0}
-                    md={0}
-                    lg={6}
-                    xl={4}
-                    className="loginPhoto m-0 p-0"
-                    >
-
-                    
-                    </Col>
-                  <Col
-                    xs={12}
-                    sm={12}
-                    md={4}
-                    lg={4}
-                    className="d-flex flex-column justify-content-center align-items-center m-0 p-0 "
-                  >
-                    
-                    <div className="loginText">
-                        LOGIN
-                    </div>
-                    <InputText
-                      type={"email"}
-                      design={
-                        credentialsError.emailError === ""
-                          ? "normalInput"
-                          : "normalInput errorInput"
-                      }
-                      placeholder={"  Email..."}
-                      name={"email"}
-                      functionHandler={inputHandler}
-                      onBlurFunction={inputCheck}
-                    />
-                    <div className="errorText">{credentialsError.emailError}</div>
-                    
-                    
-                    
-                    <InputText
-                      type={"password"}
-                      design={
-                        credentialsError.passwordError === ""
-                          ? "normalInput"
-                          : "normalInput errorInput"
-                      }
-                      placeholder={"  Contraseña..."}
-                      name={"password"}
-                      functionHandler={inputHandler}
-                      onBlurFunction={inputCheck}
-                    />
-                    <div className="errorText">
-                      {credentialsError.passwordError}
-                    </div>
-                    
-                    <div onClick={() => logMe()} className="botonLogin">
-                      Login me!
-                    </div>
-                  </Col>
-                  </div>
-                </Row>
-              </Container>
-            </div>
-          )}
+  return (
+    <div className="">
+      {welcome !== "" ? (
+        <div className="loginDesign d-flex align-items-center justify-content-center">
+          <Container>
+            <Row>
+              <Col>
+                <div className="welcomeMessage text-center"> {welcome} </div>
+              </Col>
+            </Row>
+          </Container>
         </div>
-    )
+      ) : (
+        <div className="form loginDesign">
+          <Container fluid>
+            <div className="rowLogin p-4 mb-5">
+              <Row className=" d-flex justify-content-center flex-fill p-5">
+                <Col
+                  xs={0}
+                  sm={0}
+                  md={0}
+                  lg={4}
+                  xl={4}
+                  className="d-flex justify-content-center align-items-center spaceLogin"
+                >
+                  <div className="loginPhoto"></div>
+                </Col>
+                <Col
+                  xs={12}
+                  sm={10}
+                  md={4}
+                  lg={5}
+                  className="d-flex flex-column justify-content-center align-items-center "
+                >
+                  <div className="loginText">LOGIN</div>
 
-}
+                  <InputText
+                    type={"email"}
+                    design={
+                      credentialsError.emailError === ""
+                        ? "normalInput"
+                        : "normalInput errorInput"
+                    }
+                    placeholder={"  Email..."}
+                    name={"email"}
+                    functionHandler={inputHandler}
+                    onBlurFunction={inputCheck}
+                  />
+                  <div className="errorText">{credentialsError.emailError}</div>
+
+                  <InputText
+                    type={"password"}
+                    design={
+                      credentialsError.passwordError === ""
+                        ? "normalInput "
+                        : "normalInput errorInput"
+                    }
+                    placeholder={"  Contraseña..."}
+                    name={"password"}
+                    functionHandler={inputHandler}
+                    onBlurFunction={inputCheck}
+                  />
+                  <div className="errorText">
+                    {credentialsError.passwordError}
+                  </div>
+
+                  <div onClick={() => logMe()} className="botonLogin">
+                    Login me!
+                  </div>
+
+                  {backendError !== "" ? (
+                    <div className="errorBackend">{backendError}</div>
+                  ) : null}
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </div>
+      )}
+    </div>
+  );
+};
